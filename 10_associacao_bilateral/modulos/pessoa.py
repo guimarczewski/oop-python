0668bf51d3ccde932625cf971d74b2e7fb96cbff
guimarczewski <guimarczewski@gmail.com>
2023-07-27 00:30:47 +0000

    create associacao bilateral

---
+++ 10_associacao_bilateral/modulos/pessoa.py
@@ -0,0 +1,12 @@
+class Pessoa:
+
+    def __init__(self, nome: str, local: Type[Casa]) -> None:
+        self.__local = local
+        self.__nome = nome
+
+    def entrar_no_local(self) -> None:
+        self.__local.acender_luzes()
+
+    def apresentar_local(self) -> None:
+        endereco = self.__local.get_endereco()
+        print(endereco)
